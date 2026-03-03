c2309ac789792dbc4107d9232e76fbac5482342e
Peter Almeida <55464367+hpeteralmeida@users.noreply.github.com>
2026-03-03 19:40:33 -0300

    Add user input for person registration

--- projeto estatística/cadastro.py
+++ projeto estatística/cadastro.py
@@ -1,1 +1,22 @@
-quantidade_pessoas = 
+quantidade_pessoas = int(input("Quantas pessoas você quer cadastrar? "))
+contador = 0
+nomes = []
+idades = []
+cursos = []
+while quantidade_pessoas > contador:
+  
+  print("=" * 40)
+  nome = str(input("Insira o nome da pessoa: ")).strip().upper()
+  idade = int(input("Insira a idade da pessoa: ").strip())
+  curso = str(input("Insira o nome do curso dessa pessoa: ")).strip().upper()
+  print("=" * 40)
+
+  nomes.append(nome)
+  idades.append(idade)
+  cursos.append(curso)
+  
+  contador += 1
+  
+print(nomes)
+print(idades)
+print(cursos)
