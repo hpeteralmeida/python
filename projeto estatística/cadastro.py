c12bf0424ff6155d9f125bdc4c5b1441a0ea2613
Peter Almeida <55464367+hpeteralmeida@users.noreply.github.com>
2026-03-03 21:30:19 -0300

    Update cadastro.py

--- projeto estatística/cadastro.py
+++ projeto estatística/cadastro.py
@@ -1,21 +1,35 @@
-quantidade_pessoas = int(input("Quantas pessoas você quer cadastrar? "))
+while True:
+    quantidade_pessoas = input("Quantas pessoas você quer cadastrar? ")
+    if quantidade_pessoas != '' and quantidade_pessoas.isnumeric():
+       quantidade_pessoas = int(quantidade_pessoas)
+       break
+
 contador = 0
 nomes = []
 idades = []
 cursos = []
 while quantidade_pessoas > contador:
   
-  print("=" * 40)
-  nome = str(input("Insira o nome da pessoa: ")).strip().upper()
-  idade = int(input("Insira a idade da pessoa: ").strip())
-  curso = str(input("Insira o nome do curso dessa pessoa: ")).strip().upper()
-  print("=" * 40)
-
-  nomes.append(nome)
-  idades.append(idade)
-  cursos.append(curso)
-  
-  contador += 1
+    print("=" * 40)
+    while True:
+        nome = input("Insira o nome da pessoa: ").strip().upper()
+        if nome != '' and nome.isalpha():
+            nome = str(nome)
+            nomes.append(nome)
+            break
+    while True:
+        idade = input("Insira a idade da pessoa: ").strip()
+        if idade != '' and idade.isnumeric():
+            idade = int(idade)
+            idades.append(idade)
+            break
+    while True:
+        curso = input("Insira o nome do curso dessa pessoa: ").strip().upper()
+        if curso != '' and curso.isalpha():
+            curso = str(curso)
+            cursos.append(curso)
+            break
+    contador += 1
   
 print(nomes)
 print(idades)
